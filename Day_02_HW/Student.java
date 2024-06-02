e14de894ac833f7a5bdde0d8b65101aa146a3e0b
Raghad Mohammed Aladawi <109043585+adawi-r@users.noreply.github.com>
2024-06-02 08:29:08 +0300

    Day_02_HW

---
+++ Day_02_HW/Student.java
@@ -0,0 +1,19 @@
+package Day2.Day_02_HW;
+
+public class Student {
+    private String name;
+    private int id;
+
+    public Student(String name, int id) {
+        this.name = name;
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getId() {
+        return id;
+    }
+}
